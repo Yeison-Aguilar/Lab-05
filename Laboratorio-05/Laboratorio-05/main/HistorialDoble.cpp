be9e1b8e934b5ba2942f7c0b0b320ea406977141
Yeison Hernandez Aguilar <yeison.hernandez.aguilar@est.una.ac.cr>
2026-05-13 14:35:35 -0600

    Implementa clase HistorialDoble y métodos principales
    Se creó la clase HistorialDoble con atributos y métodos para gestionar una lista doblemente enlazada de canciones. Se implementaron los métodos insertarAlInicio e insertarAlFinal (incompleto) y se declararon otros métodos de inserción, eliminación y recorrido. Se añadieron include guards al header y se limpió main.cpp.

--- Laboratorio-05/Laboratorio-05/main/HistorialDoble.cpp
+++ Laboratorio-05/Laboratorio-05/main/HistorialDoble.cpp
@@ -1,1 +1,47 @@
 #include "HistorialDoble.h"
+
+namespace EIF201 {
+
+	HistorialDoble::HistorialDoble() : cabeza(nullptr), cola(nullptr), cantidad(0) {}
+
+	HistorialDoble::HistorialDoble() {
+
+		NodoHistorial* actual = cabeza;
+
+		while (actual != nullptr) {
+			NodoHistorial* temp = actual->siguiente;
+			delete actual;
+			actual = temp;
+		}
+		cabeza = nullptr;
+		cola = nullptr;
+		std::cout << "[HitorialDoble destruido]" << std::endl;
+	}
+	
+	void HistorialDoble::insertarAlInicio(const std::string& cancion) {
+		NodoHistorial* nuevo = new NodoHistorial(cancion);
+
+		if (cabeza == nullptr) {
+			cabeza = nuevo;
+			cola = nuevo;
+		}
+		else {
+			nuevo->siguiente = cabeza;
+			cabeza->anterior = nuevo;
+			cabeza = nuevo;
+		}
+
+		cantidad++;
+	}
+	
+	void HistorialDoble::insertarAlFinal(const std::string& cancion) {
+		NodoHistorial* nuevo = new NodoHistorial(cancion);
+
+		if (cola == nullptr) {
+
+		}
+
+		
+	}
+
+}
