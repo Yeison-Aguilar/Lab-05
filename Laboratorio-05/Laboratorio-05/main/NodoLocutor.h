c60230641d03ea2fd20a29a6c470a0d1fd50c5d2
KENDALD GONZALEZ FALLAS <kendald.gonzalez.fallas@est.una.ac.cr>
2026-05-13 14:34:32 -0600

    Añade NodoLocutor y RotacionCircular; mejora NodoHistorial
    Se agrega la estructura NodoLocutor y la clase RotacionCircular para soportar listas circulares de locutores. Se reescribe NodoHistorial con constructor y destructor detallados. Se actualizan los archivos del proyecto para incluir las nuevas clases y sus implementaciones.

---
+++ Laboratorio-05/Laboratorio-05/main/NodoLocutor.h
@@ -0,0 +1,22 @@
+#ifndef NODO_LOCUTOR_H 
+#define NODO_LOCUTOR_H 
+
+#include <iostream> 
+#include <string>
+using namespace std;
+namespace EIF201 {
+	struct NodoLocutor {
+		string nombre;
+		NodoLocutor* siguiente; // en la lista circular, el último apunta al primero
+
+		NodoLocutor(const string& n)
+			: nombre(n), siguiente(nullptr) {
+			cout << "[NodoLocutor creado: "<< n << "]" << endl; 
+		}
+		~NodoLocutor() {
+			cout << "[NodoLocutor destruido: "<< nombre << "]" << endl;
+		}
+	};
+} // namespace EIF201
+#endif // NODO_LOCUTOR_H
+
