64896af8a81a29671e7c174836c5dd5132578afd
KENDALD GONZALEZ FALLAS <kendald.gonzalez.fallas@est.una.ac.cr>
2026-05-20 14:21:53 -0600

    Añadida clase Bitacora y espacio de nombres EIF201
    Se añadió la clase `Bitacora` dentro del espacio de nombres `EIF201`.
    Se implementó su constructor y se añadieron atributos privados (`Ruta`, `Validos`, `Descartados`) y métodos públicos para registrar aves, cargar registros y liberar listas.
    Se reemplazó `#pragma once` por macros de inclusión en `Bitacora.h`.
    Se incluyeron las cabeceras `NodoAve.h` y `<string>`.

--- EIF-201-lab-6/main/Bitacora.h
+++ EIF-201-lab-6/main/Bitacora.h
@@ -1,5 +1,29 @@
-#pragma once
-class Bitacora
-{
-};
+#ifndef BITACORA_H
+#define BITACORA_H
+#include"NodoAve.h"
+#include<string>
 
+
+namespace EIF201
+{
+	class Bitacora
+	{
+	private: 
+		std::string Ruta;
+		int Validos;
+		int Descartados;
+
+
+	public:
+		Bitacora(const std::string& ruta );
+		bool registrarAve(const std::string& anillo, const std::string& especie,double peso, char sexo, const std::string& fecha);
+		int cargartodas(NodoAve*& cabesa);
+		int cargasvalidas(NodoAve*& cabesa);
+		int getVAlidas() const;
+		int getDescartados() const;
+		static void lieberarlista(NodoAve*& cabesa);
+
+
+	};
+}
+#endif
